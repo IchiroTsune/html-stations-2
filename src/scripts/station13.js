f48f0246b93ea34e79580431ccb96e1acc1385a9
IchiroTsune <tsunejii16@gmail.com>
2023-07-04 22:33:51 +0900

    main

--- src/scripts/station13.js
+++ src/scripts/station13.js
@@ -1,4 +1,19 @@
 function changeBackgroundColor(e) {
   // ここに背景色を変える処理を書く。
 
+  const checkBox = document.getElementById("check")
+
+  console.log(checkBox);
+  
+
+  if(checkBox.checked){
+    console.log("true");
+    document.getElementById('text').style.backgroundColor = 'red';
+  }else{
+    console.log("false");
+    document.getElementById('text').style.backgroundColor = 'white';
+  }
+  
+  
+
 }
---
+++ src/scripts/station15.js
@@ -0,0 +1,33 @@
+function displayList() {
+
+    //var fruits = document.getElementById("fruits");
+    //var ulElement =document.createElement("ul");
+
+    //var fruit = document.querySelectorAll("p");
+    //console.log(fruit);
+
+    //for(var i = 0; i<fruit.length; i++){
+      //  var fruitsList = document.createElement("li ");
+        //fruitsList.textContent = fruit[i].textContent;
+       // ulElement.appendChild(fruitsList);
+    //}
+
+    fruits.innerHTML = "<ul>"+
+                        "<li>リンゴ</li>"+
+                        "<li>イチゴ</li>"+
+                        "<li>ブドウ</li>"+
+                        "</ul>"
+
+
+
+
+    //fruits.replaceChild(ulElement, fruit);
+
+    //console.log(ulElement);
+
+    //fruits.replaceWith(ulElement);
+
+    //ulElementがNodeタイプじゃないからうまく読み取ることができない見たい
+
+
+}
